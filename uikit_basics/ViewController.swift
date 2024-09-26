ca127cc2a4092df92db078b7c98abba25e67ff31
Daniel Anguiano <guilledaniel22@gmail.com>
2024-09-26 12:58:20 -0400

    Task: lifecycle for the viewController

--- uikit_basics/ViewController.swift
+++ uikit_basics/ViewController.swift
@@ -8,10 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBAction func tapped(_ sender: Any) {
+        let detailVC = DetailViewController()
+        navigationController?.pushViewController(detailVC, animated: true)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print("View did load")
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        print("View will appear")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        print("View did appear")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        print("View will disappear")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        print("View did disappear")
     }
 
 }
